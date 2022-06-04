0f9f4d93b1eed6fbb265555b0dcd47ce8a99d2c7
Razija16 <efendirarazija@gmail.com>
2022-06-04 14:48:58 +0200

    update app.js
    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import SignUp from './screens/signup';
+import Login from './screens/login';
+import { BrowserRouter, Route, Routes, HashRouter, Link } from "react-router-dom";
+import UserProfile from './screens/userProfile';
+import UserContext from './context';
+import NotFound from './screens/notFound';
 
 export default function App() {
-  const [background, setBackground] = useState('83, 8, 8');
 
-  useEffect(() => {
-    document.body.style.background = `rgb(${background})`
-  }, [background]);
-  
-  
+  console.log("Cordova:", window.cordova)
+  // if (window.cordova") Router = HashRouter;
+
+  const [user, setUser] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    user_id: null,
+    caseId: null
+  })
+
   return (
-    <button
-      onClick={()=>setBackground(`${Math.round(Math.random()*255)},${Math.round(Math.random()*255)},${Math.round(Math.random()*255)}`)}
-    >
-      CLICK
-    </button>
+    <UserContext.Provider value={{
+      user,
+      setUser
+    }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/user-profile' element={<UserProfile />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+
   )
 }
