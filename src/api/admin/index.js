acd1100ea33b42d58b9c910aa449a5eeaeb7ef4a
Razija16 <efendirarazija@gmail.com>
2022-06-12 22:07:00 +0200

    api functions added

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -35,7 +35,44 @@ class AdminApi {
                 "Content-Type": "application/json",
                 "Accept": "application/json"
             },
+            body: JSON.stringify(data)
+        })
+    }
+    static async getTeam(id) {
+        return fetch(`${this.BACKEND_URL}/team/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+    }
+    static async addMember(data) {
+        return fetch(`${this.BACKEND_URL}/addmember`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
             body:JSON.stringify(data)
+        })
+    }
+    static async deleteTeamMember(id) {
+        return fetch(`${this.BACKEND_URL}/deletemember/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+    }
+    static async deleteTeam(id) {
+        return fetch(`${this.BACKEND_URL}/deleteteam/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
         })
     }
 }
