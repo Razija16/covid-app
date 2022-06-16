8a28ab4d0b14ab0b56d5f89d422ed7fcc6848a9d
Razija16 <efendirarazija@gmail.com>
2022-06-16 15:21:16 +0200

    not found update

--- src/screens/notFound/index.js
+++ src/screens/notFound/index.js
@@ -5,8 +5,9 @@ const NotFound = () => {
   const navigate = useNavigate()
   return (
     <div className="not-found">
-      <h2>Welcome</h2>
-      <div className='login-button' onClick={() => { navigate('/login') }}>GO TO LOGIN</div>
+      <h2>Welcome to the Covid App</h2>
+      <div className='login-button' onClick={() => { navigate('/signup') }}>Sign Up</div>
+      <div className='login-button' onClick={() => { navigate('/login') }}>Login</div>
     </div>
   )
 }
